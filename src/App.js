607bd8f67f7f842b9f7527ea19df9e065ab780dc
BAssal <baslm8496@gmail.com>
2023-11-26 11:06:29 +0200

    uploading files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpensiveItem from "./components/ExpensiveItem";
+
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpensiveItem>this is also visable</ExpensiveItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpensiveItem.js
@@ -0,0 +1,4 @@
+function ExpensiveItem() {
+  return <h2> this is my like anyone in this palnt</h2>;
+}
+export default ExpensiveItem;
